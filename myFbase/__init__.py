228798dff364447701d1e8f20da45abd36c3953b
victor141516 <yomellamovictor@gmail.com>
2018-12-03 15:25:27 +0100

    first commit

--- myFbase/__init__.py
+++ myFbase/__init__.py
@@ -1,1 +1,12 @@
+"""
+Change to/from any base you like
+Example:
+    from myFbase import MyFbase
+
+    mfb = MyFbase('abc')
+    print(mfb.encode(8))  # cc
+    print(mfb.decode('cc'))  # 8
+"""
+
 from .myfbase import MyFbase
+__version__ = "0.0.1"
